0ecf697a6ea0601eff45bca4ce50635e57ea2d48
Полина Микава < >
2016-07-04 23:32:29 +0300

    It's work!

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-from ctypes import windll, c_int
 from ctypes import cdll
-from ctypes import WINFUNCTYPE
 
-windll.kernel32.GetModuleHandleW(0)
-
-lib = cdll.LoadLibrary('ConsoleApplication1.dll')
-mul=lib.mul
+basics = cdll.LoadLibrary('ConsoleApplication1.dll')
+mul=basics.mul
 
 test=mul(2, 3)
 
